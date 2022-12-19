ff1b4ea0966403ad290a0e6ccc84babe01eda933
Mateusz Jankowski <30898738+JankesMateusz@users.noreply.github.com>
2022-12-19 17:41:44 +0100

    uploadTender method added

--- src/main/java/com/jankes/tendersApp/tenders/TenderService.java
+++ src/main/java/com/jankes/tendersApp/tenders/TenderService.java
@@ -2,7 +2,9 @@ package com.jankes.tendersApp.tenders;
 
 import org.springframework.stereotype.Service;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 @Service
@@ -42,7 +44,47 @@ public class TenderService {
     }
 
     Tender updateTender(Tender toSave){
-        return null; //TODO
+        return tenderRepository.findById(toSave.getId()).map(existingTender -> {
+            Set<TenderItem> itemsToRemove = new HashSet<>();
+            existingTender.setLink(toSave.getLink());
+            existingTender.setPublicationDate(toSave.getPublicationDate());
+            existingTender.setBidDate(toSave.getBidDate());
+            existingTender.setTitle(toSave.getTitle());
+            existingTender.getTenderItems()
+                    .forEach(existingItem -> toSave.getTenderItems().stream()
+                            .filter(potentialOverride -> existingItem.getId().equals(toSave.getId()))
+                            .findFirst()
+                            .ifPresentOrElse(
+                                    overrideItem -> {
+                                        existingItem.setCategory(overrideItem.getCategory());
+                                        existingItem.setQuantity(overrideItem.getQuantity());
+                                        existingItem.setOs(overrideItem.getOs()); //TODO: to consider
+                                        existingItem.setOffice(overrideItem.getOffice()); // TODO: to consider
+                                        existingItem.setRemarks(overrideItem.getRemarks());
+                                        existingItem.setTaskNumber(overrideItem.getTaskNumber());
+                                    },
+                                    () -> itemsToRemove.add(existingItem)
+                            )
+                    );
+            itemsToRemove.forEach(toRemove ->{
+                existingTender.removeTenderItem(toRemove);
+                tenderItemRepository.delete(toRemove);
+            });
+            toSave.getTenderItems().stream()
+                    .filter(newItem -> existingTender.getTenderItems().stream()
+                            .noneMatch(existingItem -> existingItem.getId().equals(newItem.getId())))
+                    .collect(Collectors.toSet())
+                    .forEach(existingTender::addTenderItem);
+            tenderRepository.save(existingTender);
+            return existingTender;
+        }).orElseGet(() -> {
+            toSave.getTenderItems().forEach(item -> {
+                if(item.getTender() == null){
+                    item.setTender(toSave);                      //TODO: czy to ma sens?
+                }
+            });
+            return tenderRepository.save(toSave);
+        });
     }
 
 }
