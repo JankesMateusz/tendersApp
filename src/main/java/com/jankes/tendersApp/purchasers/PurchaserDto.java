e9452bfe7f73aa9c537b41e3415e13b8dcaeedc8
Mateusz Jankowski <30898738+JankesMateusz@users.noreply.github.com>
2022-12-28 16:27:44 +0100

    builder added to purchaserDto

--- src/main/java/com/jankes/tendersApp/purchasers/PurchaserDto.java
+++ src/main/java/com/jankes/tendersApp/purchasers/PurchaserDto.java
@@ -2,113 +2,150 @@ package com.jankes.tendersApp.purchasers;
 
 public class PurchaserDto {
 
-    private Long id;
-    private String officialName;
-    private String city;
-    private String province;
-    private TypeOfAccount typeOfAccount;
-    private String address;
-    private String zipCode;
-    private String personOfContactFirstName;
-    private String personOfContactLastName;
-    private String phone;
-    private String email;
+    private final Long id;
+    private final String officialName;
+    private final String city;
+    private final String province;
+    private final String address;
+    private final String zipCode;
+    private final String personOfContactFirstName;
+    private final String typeOfAccount;
+    private final String personOfContactLastName;
+    private final String phone;
+    private final String email;
 
-
-    public PurchaserDto(Purchaser purchaser){
-        this.id = purchaser.getId();
-        this.officialName = purchaser.getOfficialName();
-        this.city = purchaser.getCity();
-        this.province = purchaser.getProvince();
-        this.typeOfAccount = purchaser.getTypeOfAccount();
-        this.address = purchaser.getAddress();
-        this.zipCode = purchaser.getZipCode();
-        this.personOfContactFirstName = purchaser.getPersonOfContactFirstName();
-        this.personOfContactLastName = purchaser.getPersonOfContactFirstName();
-        this.phone = purchaser.getPhoneNumber();
-        this.email = purchaser.getEmail();
+    public static Builder builder() {
+        return new Builder();
     }
 
-    public Long getId(){
+    public PurchaserDto(Builder builder){
+        this.id = builder.id;
+        this.officialName = builder.officialName;
+        this.city = builder.city;
+        this.province = builder.province;
+        this.typeOfAccount = builder.typeOfAccount;
+        this.address = builder.address;
+        this.zipCode = builder.zipCode;
+        this.personOfContactFirstName = builder.personOfContactFirstName;
+        this.personOfContactLastName = builder.personOfContactLastName;
+        this.phone = builder.phone;
+        this.email = builder.email;
+    }
+
+    public Long getId() {
         return id;
     }
+
     public String getOfficialName() {
         return officialName;
-    }
-
-    public void setOfficialName(String officialName) {
-        this.officialName = officialName;
     }
 
     public String getCity() {
         return city;
     }
 
-    public void setCity(String city) {
-        this.city = city;
-    }
-
     public String getProvince() {
         return province;
-    }
-
-    public void setProvince(String province) {
-        this.province = province;
-    }
-
-    public TypeOfAccount getTypeOfAccount() {
-        return typeOfAccount;
-    }
-
-    public void setTypeOfAccount(TypeOfAccount typeOfAccount) {
-        this.typeOfAccount = typeOfAccount;
     }
 
     public String getAddress() {
         return address;
     }
 
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
     public String getZipCode() {
         return zipCode;
-    }
-
-    public void setZipCode(String zipCode) {
-        this.zipCode = zipCode;
     }
 
     public String getPersonOfContactFirstName() {
         return personOfContactFirstName;
     }
 
-    public void setPersonOfContactFirstName(String personOfContactFirstName) {
-        this.personOfContactFirstName = personOfContactFirstName;
+    public String getTypeOfAccount() {
+        return typeOfAccount;
     }
 
     public String getPersonOfContactLastName() {
         return personOfContactLastName;
     }
 
-    public void setPersonOfContactLastName(String personOfContactLastName) {
-        this.personOfContactLastName = personOfContactLastName;
-    }
-
     public String getPhone() {
         return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
     }
 
     public String getEmail() {
         return email;
     }
 
-    public void setEmail(String email) {
-        this.email = email;
+    public static class Builder {
+        private long id;
+        private String officialName;
+        private String city;
+        private String province;
+        private String typeOfAccount;
+        private String address;
+        private String zipCode;
+        private String personOfContactFirstName;
+        private String personOfContactLastName;
+        private String phone;
+        private String email;
+
+        public Builder withId(long id){
+            this.id = id;
+            return this;
+        }
+
+        public Builder withOfficialName(String officialName){
+            this.officialName = officialName;
+            return this;
+        }
+
+        public Builder withCity(String city){
+            this.city = city;
+            return this;
+        }
+
+        public Builder withProvince(String province){
+            this.province = province;
+            return this;
+        }
+
+        public Builder withTypeOfAccount(String typeOfAccount){
+            this.typeOfAccount = typeOfAccount;
+            return this;
+        }
+
+        public Builder withAddress(String address){
+            this.address = address;
+            return this;
+        }
+
+        public Builder withZipCode(String zipCode){
+            this.zipCode = zipCode;
+            return this;
+        }
+
+        public Builder withPersonOfContactFirstName(String personOfContactFirstName){
+            this.personOfContactFirstName = personOfContactFirstName;
+            return this;
+        }
+
+        public Builder withPersonOfContactLastName(String personOfContactLastName){
+            this.personOfContactLastName = personOfContactLastName;
+            return this;
+        }
+
+        public Builder withPhone(String phone){
+            this.phone = phone;
+            return this;
+        }
+
+        public Builder withEmail(String email){
+            this.email = email;
+            return this;
+        }
+
+        public PurchaserDto build(){
+            return new PurchaserDto(this);
+        }
     }
 }
