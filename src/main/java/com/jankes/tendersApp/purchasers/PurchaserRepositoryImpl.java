270853df3dab52422693dc61d9e759a7d0a011bf
Mateusz Jankowski <30898738+JankesMateusz@users.noreply.github.com>
2022-12-31 01:11:36 +0100

    few classes made package-private

--- src/main/java/com/jankes/tendersApp/purchasers/PurchaserRepositoryImpl.java
+++ src/main/java/com/jankes/tendersApp/purchasers/PurchaserRepositoryImpl.java
@@ -4,5 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PurchaserRepositoryImpl extends PurchaserRepository, JpaRepository<Purchaser, Long> {
+interface PurchaserRepositoryImpl extends PurchaserRepository, JpaRepository<Purchaser, Long> {
 }
