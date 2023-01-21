8407595eb6d2db84b74cde96947ebd2a5f770eda
Mateusz Jankowski <30898738+JankesMateusz@users.noreply.github.com>
2023-01-22 00:21:51 +0100

    authentication filter class added

--- src/main/java/com/jankes/tendersApp/security/AuthenticationFilter.java
+++ src/main/java/com/jankes/tendersApp/security/AuthenticationFilter.java
@@ -1,6 +1,11 @@
 package com.jankes.tendersApp.security;
 
 import org.springframework.http.HttpHeaders;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.FilterChain;
@@ -14,6 +19,14 @@ public class AuthenticationFilter extends OncePerRequestFilter {
 
     private static final String BEARER = "Bearer ";
 
+    private final UserDetailsService userDetailsService;
+    private final TokenService tokenService;
+
+    public AuthenticationFilter(UserDetailsService userDetailsService, TokenService tokenService) {
+        this.userDetailsService = userDetailsService;
+        this.tokenService = tokenService;
+    }
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
 
@@ -21,6 +34,22 @@ public class AuthenticationFilter extends OncePerRequestFilter {
                 request.getHeader(HttpHeaders.AUTHORIZATION)
         ).filter(header -> header.startsWith(BEARER))
                 .map(header -> header.substring(BEARER.length()))
-                .ifPresent();
+                .ifPresent(token -> {
+                    if(SecurityContextHolder.getContext().getAuthentication() != null){
+                        return;
+                    }
+                    String username = tokenService.getUsernameFromToken(token);
+                    UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+                    if(tokenService.isValidForUser(token, userDetails)){
+                        var authentication = new UsernamePasswordAuthenticationToken( // TODO: not sure about these
+                                userDetails.getUsername(),
+                                userDetails.getPassword(),
+                                userDetails.getAuthorities()
+                        );
+                        authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                        SecurityContextHolder.getContext().setAuthentication(authentication);
+                    }
+                });
+        filterChain.doFilter(request, response);
     }
 }
