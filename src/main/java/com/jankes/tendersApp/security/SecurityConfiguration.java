827d97ac86e9cc3b7fbfb323b71a5d109f4be874
Mateusz Jankowski <30898738+JankesMateusz@users.noreply.github.com>
2023-02-27 17:43:45 +0100

    security config

--- src/main/java/com/jankes/tendersApp/security/SecurityConfiguration.java
+++ src/main/java/com/jankes/tendersApp/security/SecurityConfiguration.java
@@ -1,8 +1,42 @@
 package com.jankes.tendersApp.security;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
 public class SecurityConfiguration  {
+
+    private final AuthenticationFilter authenticationFilter;
+    private final AuthenticationProvider authenticationProvider;
+
+
+    public SecurityConfiguration(AuthenticationFilter authenticationFilter, AuthenticationProvider authenticationProvider) {
+        this.authenticationFilter = authenticationFilter;
+        this.authenticationProvider = authenticationProvider;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        http
+                .csrf()
+                .disable()
+                .authorizeHttpRequests()
+                .requestMatchers()
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authenticationProvider(authenticationProvider)
+                .addFilterBefore(authenticationFilter, UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
 }
